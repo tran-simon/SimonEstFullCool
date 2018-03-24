f1035581527268498728bdaf05fab4f01475f5ed
root <root@localhost.localdomain>
2018-03-24 01:12:49 -0400

    add SharedCount class

---
+++ src/main/java/SharedCounts.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author root
+ */
+public class SharedCounts {
+    private String url;
+    private final String DOMAIN = "https://api.sharedcount.com/v1.0/";
+    private final String APIKEY = "83fcad976cc23fc8868c7d4e9d78994f8b887fce";
+    
+    
+    public SharedCounts(String url) {
+        this.url = url;
+        
+    }
+    
+    public int getNbClick() {
+        
+        return 0;
+    }
+}
