bc15f79ee517231a11119b5684be67bd59e7ba9b
lool01 <lool45162@gmail.com>
2018-03-24 10:32:32 -0400

    Fonction pour insérer les valeurs dans la db sql. Fonction static (tu fais Connection.queryInsert(String keywors, String titre, int click))

---
+++ src/main/java/Connection.java
@@ -0,0 +1,41 @@
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Connection {
+
+    public static void main(String[] arsg){
+        queryInsert("test,test1", "Les test sont des test qui test", 100);
+    }
+
+    public static void queryInsert(String keywords, String titre, int click){
+        String hostName = "testai1.database.windows.net";
+        String dbName = "leo";
+        String user = "leo";
+        String password = "qwerty1234!";
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+        java.sql.Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url);
+
+            // Create and execute a SELECT SQL statement.
+            String selectSql = String.format("INSERT INTO data(keywords, click, titre) VALUES(\'%s\', %d, \'%s\');", keywords, click, titre);
+
+            /*try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(selectSql)) {
+
+                // Print results from select statement
+                while (resultSet.next())
+                {
+                    System.out.println(resultSet.getString(1) + " "
+                            + resultSet.getString(2));
+                }
+                connection.close();
+            }*/
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
