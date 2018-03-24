1999db164ec2833e53eeaa9ba3aa1d16611e03ee
simon_tran@icloud.com <simon_tran@icloud.com>
2018-03-24 00:53:17 -0400

    Added Lineup class

---
+++ src/main/java/Lineup.java
@@ -0,0 +1,43 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Lineup extends JSONObject{
+    public Lineup(String link){
+        super(getBody(link));
+    }
+
+    public static String getBody(String link) {
+        String bodyString = "";
+        try {
+            HttpResponse<String> response = Unirest.get(link)
+                    .header("Authorization", "Client-Key bf9ac6d8-9ad8-4124-a63c-7b7bdf22a2ee")
+                    .header("Cache-Control", "no-cache")
+                    .header("Postman-Token", "8064e4a8-c861-48d8-9fd5-bc8ce25e0960")
+                    .asString();
+            bodyString = response.getBody();
+
+        } catch (Exception e) {
+            bodyString = "";
+        }
+        if (bodyString.equals("")) {
+            System.out.println("USING TESTING BODY");
+            bodyString = Main.readFile("testingBody.txt");
+        }
+        return bodyString;
+    }
+
+    public JSONArray getItems() {
+        JSONObject pagedList = this.getJSONObject("pagedList");
+        JSONArray jsonArray = pagedList.getJSONArray("items");
+        ArrayList<JSONObject> objectList = new ArrayList();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            objectList.add(jsonArray.getJSONObject(i));
+        }
+
+        return jsonArray;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,7 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static JSONArray getItems(String bodyString) {
-        JSONObject body = new JSONObject(bodyString);
-        JSONObject pagedList = body.getJSONObject("pagedList");
-        JSONArray jsonArray = pagedList.getJSONArray("items");
-        ArrayList<JSONObject> objectList = new ArrayList();
-        for (int i = 0; i < jsonArray.length(); i++) {
-            objectList.add(jsonArray.getJSONObject(i));
-        }
 
-        return jsonArray;
-    }
 
     public static void writeCSVToFile(String csv, File file) {
         file.delete();
@@ -58,27 +48,6 @@ public class Main {
         }
     }
 
-    public static String getBodyString() {
-        String bodyString = "";
-        try {
-            HttpResponse<String> response = Unirest.get("https://services.radio-canada.ca/hackathon/neuro/v1/future/lineups/475289?pageNumber=1")
-                    .header("Authorization", "Client-Key bf9ac6d8-9ad8-4124-a63c-7b7bdf22a2ee")
-                    .header("Cache-Control", "no-cache")
-                    .header("Postman-Token", "8064e4a8-c861-48d8-9fd5-bc8ce25e0960")
-                    .asString();
-            bodyString = response.getBody();
-
-        } catch (Exception e) {
-            System.out.println("USING TESTING BODY");
-            bodyString = "";
-        }
-
-
-        if (bodyString.equals("")) {
-            bodyString = readFile("testingBody.txt");
-        }
-        return bodyString;
-    }
 
 
 
@@ -87,11 +56,14 @@ public class Main {
         return items.getJSONObject(id).getJSONObject("selfLink").getString("href");
     }
     public static void main(String[] args) {
-        JSONArray items = getItems(getBodyString());
+
+        String link = "https://services.radio-canada.ca/hackathon/neuro/v1/future/lineups/475289?pageNumber=1";
+        Lineup lineup = new Lineup(link);
+        JSONArray items = lineup.getItems();
         String path = "items.csv";
         writeCSVToFile(CDL.toString(items), new File(path));
 
-        String link = getLink(items, 1);
+        link = getLink(items, 0);
         News news = new News(link);
         System.out.println(news.toString());
         System.out.println(news.getTitle());
